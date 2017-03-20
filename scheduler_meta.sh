160654bd536fd5eeda451e865a6e970669e57586
Stephan Jennewein <stephan@selfnet.de>
2017-03-20 23:42:23 +0100

    wrong foldername

--- scheduler_meta.sh
+++ scheduler_meta.sh
@@ -5,10 +5,10 @@ then
   for f in ~/papers/dois*.txt
   do
     touch ~/papers/RUNNING
-    /usr/bin/python3 $f /home/stephan/papers/metadata
-    tar -xzf /home/stephan/papers/metadata_$f.tar.gz /home/stephan/papers/metadata
+    /usr/bin/python3 $f /home/xt3/papers/metadata
+    tar -xzf /home/xt3/papers/metadata_$f.tar.gz /home/xt3/papers/metadata
     rm $f
-    rm -rf /home/stephan/papers/metadata/*
+    rm -rf /home/xt3/papers/metadata/*
     touch ~/papers/FINISHED
     rm ~/papers/RUNNING
   done
