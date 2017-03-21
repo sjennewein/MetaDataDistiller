1fda4fb32d0754ca87cf87003c0cd6879765b5de
Stephan Jennewein <stephan@selfnet.de>
2017-03-21 12:21:45 +0100

    working

--- scheduler_meta.sh
+++ scheduler_meta.sh
@@ -5,12 +5,13 @@ then
   for f in ~/papers/dois*.txt
   do
     echo $f
+    NAME=$(echo $f  |cut -d _ -f 2 | cut -d . -f 1)
     touch ~/papers/RUNNING
     /usr/bin/python3 /home/xt3/MetaDataDistiller/metadata_fromList.py $f /home/xt3/papers/metadata/
-    #tar -xzf /home/xt3/papers/metadata_$f.tar.gz /home/xt3/papers/metadata
-    #rm $f
-    #rm -rf /home/xt3/papers/metadata/*
-    #touch ~/papers/FINISHED
-    #rm ~/papers/RUNNING
+    tar -czf /home/xt3/papers/metadata_$NAME.tar.gz /home/xt3/papers/metadata
+    rm $f
+    rm -rf /home/xt3/papers/metadata/*
+    touch ~/papers/FINISHED
+    rm ~/papers/RUNNING
   done
 fi 
