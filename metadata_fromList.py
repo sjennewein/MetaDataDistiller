5bd6ea27e053bd7386068ee1f89f17ba232c4a2d
Stephan Jennewein <stephan@selfnet.de>
2017-03-26 15:55:48 +0200

    no print output

--- metadata_fromList.py
+++ metadata_fromList.py
@@ -65,6 +65,6 @@ for doi in content:
             out = payload.PayloadEncoder().encode(metadata)
         json_file.write(out)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # print("--- %s seconds ---" % (time.time() - start_time))
 
 # print("--- TOTAL: %s seconds ---" % (time.time() - total_time))
