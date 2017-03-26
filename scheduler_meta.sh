3102fb83913626309f3f08824ce71093f48e74b2
Stephan Jennewein <stephan@selfnet.de>
2017-03-26 13:04:19 +0200

    fixed file checking in bash

--- scheduler_meta.sh
+++ scheduler_meta.sh
@@ -3,9 +3,10 @@ set -e
 
 if [ ! -f ~/papers/RUNNING ] 
 then
-  for f in ~/papers/dois*.txt
-  do
-    echo $f
+  files=(~/papers/dois*.txt)
+  if [ -e "${files[0]}" ]
+  then
+    f = ${files[0]}
     NAME=$(echo $f  |cut -d _ -f 2 | cut -d . -f 1)
     touch ~/papers/RUNNING
     /usr/bin/python3 /home/xt3/MetaDataDistiller/metadata_fromList.py $f /home/xt3/papers/metadata/
@@ -14,5 +15,5 @@ then
     rm -rf /home/xt3/papers/metadata/*
     touch ~/papers/FINISHED
     rm ~/papers/RUNNING
-  done
+  fi 
 fi 
