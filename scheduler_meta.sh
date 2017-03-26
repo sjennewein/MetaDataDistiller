a78759e0f216dc69a81fdab3f43a33d236eb157d
Stephan Jennewein <stephan@selfnet.de>
2017-03-26 13:56:13 +0200

    stupid bash bug for variable assignment

--- scheduler_meta.sh
+++ scheduler_meta.sh
@@ -6,7 +6,7 @@ then
   files=(~/papers/dois*.txt)
   if [ -e "${files[0]}" ]
   then
-    f = ${files[0]}
+    f=${files[0]}
     NAME=$(echo $f  |cut -d _ -f 2 | cut -d . -f 1)
     touch ~/papers/RUNNING
     /usr/bin/python3 /home/xt3/MetaDataDistiller/metadata_fromList.py $f /home/xt3/papers/metadata/
